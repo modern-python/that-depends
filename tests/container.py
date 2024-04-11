aa6d05e5f7a12e63f48d9ca9a61490a71beed11e
Lev Vereshchagin <mail@vrslev.com>
2024-04-11 14:29:04 +0300

    Infer provider types from creators

--- tests/container.py
+++ tests/container.py
@@ -44,9 +44,9 @@ class SingletonFactory:
 
 
 class DIContainer(BaseContainer):
-    sync_resource = providers.Resource[str](create_sync_resource)
-    async_resource = providers.AsyncResource[str](create_async_resource)
-    sequence = providers.List[str](sync_resource, async_resource)
+    sync_resource = providers.Resource(create_sync_resource)
+    async_resource = providers.AsyncResource(create_async_resource)
+    sequence = providers.List(sync_resource, async_resource)
 
     independent_factory = providers.Factory(IndependentFactory, dep1="text", dep2=123)
     sync_dependent_factory = providers.Factory(
