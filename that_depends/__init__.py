493eec1a47b3343435db27f13f4a04f2c39ea4d4
Artur Shiriev <lesnik512@gmail.com>
2024-05-25 10:50:02 +0300

    add injection to sync functions

--- that_depends/__init__.py
+++ that_depends/__init__.py
@@ -1,6 +1,6 @@
 from that_depends import providers
 from that_depends.container import BaseContainer
-from that_depends.injection import Provide, inject
+from that_depends.injection import Provide, inject, inject_to_sync
 
 
-__all__ = ["providers", "BaseContainer", "inject", "Provide"]
+__all__ = ["providers", "BaseContainer", "inject", "inject_to_sync", "Provide"]
