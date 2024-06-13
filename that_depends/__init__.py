c817284d86f799fef70d187ff4f1af523cedeadc
Lev Vereshchagin <mail@vrslev.com>
2024-06-13 13:43:15 +0300

    Fix linting

--- that_depends/__init__.py
+++ that_depends/__init__.py
@@ -1,7 +1,7 @@
 from that_depends import providers
-from that_depends.providers import container_context
 from that_depends.container import BaseContainer
 from that_depends.injection import Provide, inject
+from that_depends.providers import container_context
 
 
 __all__ = ["container_context", "providers", "BaseContainer", "inject", "Provide"]
