94cb01d392bf115f77c244330c90fd99966d0bf1
ivan <ivan.belyaev@ailet.com>
2024-07-15 20:19:18 +0300

    Separate tests with batch and single provider overriding

--- tests/providers/test_providers_overriding.py
+++ tests/providers/test_providers_overriding.py
@@ -5,7 +5,7 @@ import pytest
 from tests import container
 
 
-async def test_providers_overriding() -> None:
+async def test_batch_providers_overriding() -> None:
     async_resource_mock = datetime.datetime.fromisoformat("2023-01-01")
     sync_resource_mock = datetime.datetime.fromisoformat("2024-01-01")
     async_factory_mock = datetime.datetime.fromisoformat("2025-01-01")
@@ -36,7 +36,7 @@ async def test_providers_overriding() -> None:
     assert (await container.DIContainer.async_resource()) != async_resource_mock
 
 
-async def test_providers_overriding_sync_resolve() -> None:
+async def test_batch_providers_overriding_sync_resolve() -> None:
     async_resource_mock = datetime.datetime.fromisoformat("2023-01-01")
     sync_resource_mock = datetime.datetime.fromisoformat("2024-01-01")
     simple_factory_mock = container.SimpleFactory(dep1="override", dep2=999)
@@ -80,3 +80,56 @@ def test_providers_overriding_fail_with_unknown_provider() -> None:
 
     with pytest.raises(RuntimeError, match=match), container.DIContainer.override_providers(providers_for_overriding):
         ...
+
+
+async def test_providers_overriding() -> None:
+    async_resource_mock = datetime.datetime.fromisoformat("2023-01-01")
+    sync_resource_mock = datetime.datetime.fromisoformat("2024-01-01")
+    async_factory_mock = datetime.datetime.fromisoformat("2025-01-01")
+    simple_factory_mock = container.SimpleFactory(dep1="override", dep2=999)
+    singleton_mock = container.SingletonFactory(dep1=False)
+    container.DIContainer.async_resource.override(async_resource_mock)
+    container.DIContainer.sync_resource.override(sync_resource_mock)
+    container.DIContainer.simple_factory.override(simple_factory_mock)
+    container.DIContainer.singleton.override(singleton_mock)
+    container.DIContainer.async_factory.override(async_factory_mock)
+
+    await container.DIContainer.simple_factory()
+    dependent_factory = await container.DIContainer.dependent_factory()
+    singleton = await container.DIContainer.singleton()
+    async_factory = await container.DIContainer.async_factory()
+
+    assert dependent_factory.simple_factory.dep1 == simple_factory_mock.dep1
+    assert dependent_factory.simple_factory.dep2 == simple_factory_mock.dep2
+    assert dependent_factory.sync_resource == sync_resource_mock
+    assert dependent_factory.async_resource == async_resource_mock
+    assert singleton is singleton_mock
+    assert async_factory is async_factory_mock
+
+    container.DIContainer.reset_override()
+    assert (await container.DIContainer.async_resource()) != async_resource_mock
+
+
+async def test_providers_overriding_sync_resolve() -> None:
+    async_resource_mock = datetime.datetime.fromisoformat("2023-01-01")
+    sync_resource_mock = datetime.datetime.fromisoformat("2024-01-01")
+    simple_factory_mock = container.SimpleFactory(dep1="override", dep2=999)
+    singleton_mock = container.SingletonFactory(dep1=False)
+    container.DIContainer.async_resource.override(async_resource_mock)
+    container.DIContainer.sync_resource.override(sync_resource_mock)
+    container.DIContainer.simple_factory.override(simple_factory_mock)
+    container.DIContainer.singleton.override(singleton_mock)
+
+    container.DIContainer.simple_factory.sync_resolve()
+    await container.DIContainer.async_resource.async_resolve()
+    dependent_factory = container.DIContainer.dependent_factory.sync_resolve()
+    singleton = container.DIContainer.singleton.sync_resolve()
+
+    assert dependent_factory.simple_factory.dep1 == simple_factory_mock.dep1
+    assert dependent_factory.simple_factory.dep2 == simple_factory_mock.dep2
+    assert dependent_factory.sync_resource == sync_resource_mock
+    assert dependent_factory.async_resource == async_resource_mock
+    assert singleton is singleton_mock
+
+    container.DIContainer.reset_override()
+    assert container.DIContainer.sync_resource.sync_resolve() != sync_resource_mock
