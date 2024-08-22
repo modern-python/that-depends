3f997f813b04bd58f6fd8f32e6ed290241a77c24
Artur Shiriev <lesnik512@gmail.com>
2024-08-22 23:56:01 +0300

    fix tests

--- tests/tricky_cases/test_race_condition.py
+++ tests/tricky_cases/test_race_condition.py
@@ -2,26 +2,25 @@ import asyncio
 import typing
 
 import that_depends
-from that_depends import container_context
-from that_depends.providers import ContextResource, Resource
+from that_depends import container_context, providers
 
 
 async def test_race_condition_in_resource() -> None:
     calls: int = 0
 
-    async def create_client() -> typing.AsyncIterator[str]:
+    async def create_resource() -> typing.AsyncIterator[str]:
         nonlocal calls
         calls += 1
         await asyncio.sleep(0)
         yield ""
 
     class IOCContainer(that_depends.BaseContainer):
-        client = Resource(create_client)
+        resource = providers.Resource(create_resource)
 
     @that_depends.inject
-    async def uses_client(_: str = that_depends.Provide[IOCContainer.client]) -> None: ...
+    async def uses_resource(_: str = that_depends.Provide[IOCContainer.resource]) -> None: ...
 
-    await asyncio.gather(uses_client(), uses_client())
+    await asyncio.gather(uses_resource(), uses_resource())
 
     assert calls == 1
 
@@ -36,7 +35,7 @@ async def test_race_condition_in_async_resource() -> None:
         yield ""
 
     class IOCContainer(that_depends.BaseContainer):
-        client = ContextResource(create_client)
+        client = providers.ContextResource(create_client)
 
     @that_depends.inject
     async def uses_client(_: str = that_depends.Provide[IOCContainer.client]) -> None: ...
