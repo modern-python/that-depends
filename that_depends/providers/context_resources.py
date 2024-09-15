f8739fea0e4ec39fddd8029f25469c76d2b25eee
Alexander <e1634240@student.tuwien.ac.at>
2024-09-15 16:56:23 +0200

    #72 Refactor container_context to class.

--- that_depends/providers/context_resources.py
+++ that_depends/providers/context_resources.py
@@ -1,9 +1,10 @@
 import contextlib
+from functools import wraps
 import logging
 import typing
 import uuid
 import warnings
-from contextvars import ContextVar
+from contextvars import ContextVar, Token
 
 from that_depends.providers.base import AbstractResource, ResourceContext
 
@@ -19,7 +20,7 @@ Receive = typing.Callable[[], typing.Awaitable[Message]]
 Send = typing.Callable[[Message], typing.Awaitable[None]]
 ASGIApp = typing.Callable[[Scope, Receive, Send], typing.Awaitable[None]]
 
-
+"""
 @contextlib.asynccontextmanager
 async def container_context(initial_context: dict[str, typing.Any] | None = None) -> typing.AsyncIterator[None]:
     token: typing.Final = context_var.set(initial_context or {})
@@ -32,6 +33,44 @@ async def container_context(initial_context: dict[str, typing.Any] | None = None
                     await context_item.tear_down()
         finally:
             context_var.reset(token)
+"""
+
+
+class AsyncContextDecorator(object):
+    "A base class or mixin that enables async context managers to work as decorators."
+
+    def _recreate_cm(self):
+        """Return a recreated instance of self.
+        """
+        return self
+
+    def __call__(self, func):
+        @wraps(func)
+        async def inner(*args, **kwds):
+            async with self._recreate_cm():
+                return await func(*args, **kwds)
+        return inner
+
+
+ContextType = typing.TypeVar("ContextType", bound=typing.Dict[str, typing.Any])
+class container_context(contextlib.AbstractAsyncContextManager[ContextType], AsyncContextDecorator):
+    def __init__(self, initial_context: typing.Optional[ContextType] = None) -> None:
+        
+        self._initial_context: typing.Final[ContextType] = initial_context
+        self._context_token: typing.Optional[Token[ContextType]] = None
+
+    async def __aenter__(self) -> ContextType:
+        self._context_token = context_var.set(self._initial_context or {})
+        return context_var.get()
+
+
+    async def __aexit__(self, exc_type, exc_val, traceback) -> None:
+        try:
+            for context_item in reversed(context_var.get().values()):
+                if isinstance(context_item, ResourceContext):
+                    await context_item.tear_down()
+        finally:
+            context_var.reset(self._context_token)
 
 
 class DIContextMiddleware:
