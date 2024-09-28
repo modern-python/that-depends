25bce8906a6753c2bc984fd976df086d8e54ff0c
artur.shiriev <artur.shiriev@raiffeisen.ru>
2024-09-28 20:50:53 +0300

    revert to unified container_context and fix error with same context var token when container_context used as decorator

--- that_depends/providers/__init__.py
+++ that_depends/providers/__init__.py
@@ -6,8 +6,6 @@ from that_depends.providers.context_resources import (
     ContextResource,
     DIContextMiddleware,
     container_context,
-    fetch_context_item,
-    sync_container_context,
 )
 from that_depends.providers.factories import AsyncFactory, Factory
 from that_depends.providers.object import Object
@@ -32,6 +30,4 @@ __all__ = [
     "Selector",
     "Singleton",
     "container_context",
-    "sync_container_context",
-    "fetch_context_item",
 ]
