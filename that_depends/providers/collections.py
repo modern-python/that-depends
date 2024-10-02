fdec1fb3a73b2b518234597401b6708f6ad56d5f
Alexander <e1634240@student.tuwien.ac.at>
2024-10-02 22:50:43 +0200

    Disable attribute resolution for collections.

--- that_depends/providers/collections.py
+++ that_depends/providers/collections.py
@@ -13,6 +13,10 @@ class List(AbstractProvider[list[T_co]]):
         super().__init__()
         self._providers: typing.Final = providers
 
+    def __getattr__(self, attr_name: str) -> typing.Any:  # noqa: ANN401
+        msg = f"'{type(self)}' object has no attribute '{attr_name}'"
+        raise AttributeError(msg)
+
     async def async_resolve(self) -> list[T_co]:
         return [await x.async_resolve() for x in self._providers]
 
@@ -29,6 +33,10 @@ class Dict(AbstractProvider[dict[str, T_co]]):
     def __init__(self, **providers: AbstractProvider[T_co]) -> None:
         super().__init__()
         self._providers: typing.Final = providers
+
+    def __getattr__(self, attr_name: str) -> typing.Any:  # noqa: ANN401
+        msg = f"'{type(self)}' object has no attribute '{attr_name}'"
+        raise AttributeError(msg)
 
     async def async_resolve(self) -> dict[str, T_co]:
         return {key: await provider.async_resolve() for key, provider in self._providers.items()}
