9720f0fa70f5470c9d9b486dd391723fd9d31051
Alexander <alexanderlazarev0@gmail.com>
2025-01-06 21:53:21 +0100

    Made BaseContainerMeta thread safe.

--- that_depends/meta.py
+++ that_depends/meta.py
@@ -1,4 +1,5 @@
 import typing
+from threading import Lock
 
 
 if typing.TYPE_CHECKING:
@@ -6,14 +7,17 @@ if typing.TYPE_CHECKING:
 
 
 class BaseContainerMeta(type):
-    instances: typing.ClassVar[list[type["BaseContainer"]]] = []
+    _instances: typing.ClassVar[list[type["BaseContainer"]]] = []
+
+    _lock: Lock = Lock()
 
     def __new__(cls, name: str, bases: tuple[type, ...], namespace: dict[str, typing.Any]) -> type:
         new_cls = super().__new__(cls, name, bases, namespace)
-        if name != "BaseContainer":
-            cls.instances.append(new_cls)  # type: ignore[arg-type]
+        with cls._lock:
+            if name != "BaseContainer":
+                cls._instances.append(new_cls)  # type: ignore[arg-type]
         return new_cls
 
     @classmethod
     def get_instances(cls) -> list[type["BaseContainer"]]:
-        return cls.instances
+        return cls._instances
